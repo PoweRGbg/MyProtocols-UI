5b0512ba4274a725e0a249e143e35109cb19fd2c
Roumen Georgiev <roumen_georgiev@yahoo.com>
2024-03-24 10:22:45 +0200

    added login & register UI only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,6 @@
 import { Component } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { PrescriptionModule } from './prescriptions/prescriptions.module';
-import { ProtocolsModule } from './protocols/protocols.module';
-import { PrescriptionService } from './prescriptions/prescriptions-service/prescriptions.service';
-import { ProtocolsService } from './protocols/protocols.service';
-
 @Component({
     selector: 'app-root',
-    standalone: true,
-    imports: [PrescriptionModule, HttpClientModule, ProtocolsModule],
-    providers: [PrescriptionService, ProtocolsService],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
