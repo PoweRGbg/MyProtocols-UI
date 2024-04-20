77d4cb36d65209f28e5cfe29c0e67b17a58f5b47
Roumen Georgiev <roumen_georgiev@yahoo.com>
2024-04-20 18:52:30 +0300

    more material

---
+++ src/app/common/common.ts
@@ -0,0 +1,10 @@
+// function to convert date to EU format
+export function convertDateToEU(date: string): string {
+  const dateObj = new Date(date);
+  return `${dateObj.getDate()}/${dateObj.getMonth() + 1}/${dateObj.getFullYear()}`;
+}
+// function to return date from string in EU format
+export function convertDateFromEU(date: string): Date {
+  const [day, month, year] = date.split('/');
+  return new Date(Number(year), Number(month) - 1, Number(day));
+}
